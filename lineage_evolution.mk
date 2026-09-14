209af0d4cad4599d4c6f56bc0c6f7dea7b6d046b
Yumi Yukimura <me.cafebabe@gmail.com>
2026-09-15 04:24:46 +0800

    cgsi: evolution: Disable FaceUnlock for non-arm64
    TARGET module FaceUnlock requires non-existent TARGET module: libFaceDetectCA
    TARGET module FaceUnlock requires non-existent TARGET module: libMegviiUnlock
    TARGET module FaceUnlock requires non-existent TARGET module: libMegviiUnlock-jni-1.2

--- lineage_evolution.mk
+++ lineage_evolution.mk
@@ -1,6 +1,11 @@
 CGSI_CUSTOM_ROM_NAME := Evolution-X
 CGSI_CUSTOM_ROM_BRANCH := cnb
 
+# FaceUnlock
+ifneq ($(CGSI_ARCH),arm64)
+TARGET_DISABLE_FACEUNLOCK := true
+endif
+
 # GMS
 ifneq ($(CGSI_ARCH),arm64)
 WITH_GMS ?= false
